fa28dcb52de4d3da5b84c4a2955eeac56ffa3882
anhle3532 <anhle3532@gmail.com>
2019-02-26 00:14:55 +0700

    client action send data and server emit data to client, then client listen and action situation

--- demo_chat/public/xuly.js
+++ demo_chat/public/xuly.js
@@ -1,5 +1,18 @@
 var socket = io("http://localhost:5000");
+socket.on("Server send register fail", function(){
+    alert("Username has been registered");
+});
+
+socket.on("Server send register success", function(data){
+    $("#curUser").html(data);
+    $("#loginForm").hide(1000);
+    $("#chatForm").show(1000);
+});
+
 $(document).ready(function(){
     $("#loginForm").show();
     $("#chatForm").hide();
+    $("#btnRegister").click(function(){
+        socket.emit("Client send username", $("#txtName").val());
+    });
 });
