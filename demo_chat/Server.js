2e4567c1c25e66f3e3c05220c73f6b7b93f589ab
anhle3532 <anhle3532@gmail.com>
2019-02-26 00:12:34 +0700

    check user register and show screen fit to situation

--- demo_chat/Server.js
+++ demo_chat/Server.js
@@ -10,8 +10,28 @@ var server = require("http").Server(app);
 var io = require("socket.io")(server);
 server.listen(5000);
 
+// Declare array to check user registered
+var arrUsers  = ["aaa"];
+
 io.on("connection", function(socket){
     console.log(socket.id+" connected");
+
+    socket.on("Client send username", function(data){
+        //console.log(data);
+
+        // Check if user having in array
+        if(arrUsers.indexOf(data) >= 0)
+        {
+            // Fail
+            socket.emit("Server send register fail");
+        }
+        else
+        {
+            // Success
+            arrUsers.push(data);
+            socket.emit("Server send register success", data);
+        }
+    });
 });
 
 app.get("/", function(req, res){
--- demo_chat/public/xuly.js
+++ demo_chat/public/xuly.js
@@ -1,4 +1,5 @@
 var socket = io("http://localhost:5000");
 $(document).ready(function(){
-    //alert(1);
+    $("#loginForm").show();
+    $("#chatForm").hide();
 });
